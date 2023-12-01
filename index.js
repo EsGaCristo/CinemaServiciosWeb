cdf5ee791861504e81c910669cf57d5f05734af5
Christopher Jair Estrada Garcia <101943800+EsGaCristo@users.noreply.github.com>
2023-11-30 19:28:22 -0700

    Just movies

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 
 require('./utils/mongoConnection')
 
-const bookRouter = require('./routers/books.router');
 const movieRouter = require('./routers/movies.router');
 const userRuter  = require('./routers/users.router');
 
@@ -19,7 +18,6 @@ app.get("/", (req, res) => {
 });
 app.use(express.json({ limit: "50mb" }));
 
-app.use('/books', bookRouter);
 app.use('/movies',movieRouter);
 app.use('/users',userRuter)
 
